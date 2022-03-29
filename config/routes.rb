197ec174a9babb12685c8a914f7c2c7a91b70d78
Mickael Toledano gh api mikatol06@gmail.com <mikatol06@gmail.com>
2022-03-29 17:22:55 +0200

    cloudinary

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :bookings
-  resources :instruments
-  resources :users
+  resources :instruments do
+    resources :bookings
+  end
 end
