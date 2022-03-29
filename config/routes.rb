177594aeaf2abe996c3c544df2be4f1df5201411
Mickael Toledano gh api mikatol06@gmail.com <mikatol06@gmail.com>
2022-03-29 17:46:07 +0200

    instru

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'instruments#index'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :instruments do
+  resources :instruments, except: :index do
     resources :bookings
   end
 end
