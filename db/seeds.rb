88693fdb42852ab83b2937898f8894c66080af8e
zied oussaifi <ousaifizied@hotmail.fr>
2022-03-30 11:33:41 +0200

    raplace dates in bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@instruments = Instrument.create([{ name: 'guitar', price: '100.0'}])
